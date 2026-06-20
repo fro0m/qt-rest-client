8f7688e987589681afcbf5437d3051b8c868de5a
Konstantin Frumkin <fro0m.spam@gmail.com>
2026-06-20 16:55:12 +0300

    turn main.cpp into a working example instead of a broken stub
    main.cpp included "mainwindowe.h" (note the typo) and instantiated a
    MainWindowe class that has never existed in this repo. It wasn't referenced by
    any build file either, so it just sat there not compiling.
    
    Replaced it with a small console example that does a real GET against the
    postman echo endpoint and prints status + body, which is actually useful for
    seeing how the library is meant to be used.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
-#include "mainwindowe.h"
+// Minimal example: GET https://postman-echo.com/get and print the response.
+//
+// The previous main.cpp referenced a MainWindow class that was never part of
+// this project, so it couldn't compile. This is a small, self-contained
+// console example that actually exercises the library.
+
 #include "HttpRequestWorker.h"
-#include <QApplication>
 
-int main(int argc, char *argv[])
+#include <QCoreApplication>
+#include <QDebug>
+#include <QEventLoop>
+
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
+    QCoreApplication app(argc, argv);
 
-    MainWindowe w;
-    w.show();
+    HttpRequestInput input(QStringLiteral("https://postman-echo.com/get?foo1=bar1&foo2=bar2"),
+                           HttpMethod::Get);
 
-    return a.exec();
+    HttpRequestWorker worker;
+    worker.setReadResponseOnError(true);
+    worker.execute(&input);
+
+    QEventLoop loop;
+    QObject::connect(&worker, &HttpRequestWorker::executionFinished,
+                     [&loop](HttpRequestWorker* w) {
+                         qDebug().noquote() << "status:" << w->statusCode;
+                         qDebug().noquote() << "body:" << w->response;
+                         if (w->errorType != QNetworkReply::NoError)
+                             qDebug().noquote() << "error:" << w->errorStr;
+                         loop.quit();
+                     });
+    loop.exec();
+
+    return worker.errorType == QNetworkReply::NoError ? 0 : 1;
 }
